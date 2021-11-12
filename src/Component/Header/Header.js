cfb011e650688c4e25453a600e639bafb8a62359
MD.Kamrul Hasan <83531333+Kamrul-CSE-47@users.noreply.github.com>
2021-11-12 12:05:38 +0600

    first commit

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Basmala—Allah, ar-Rahman and ar-Rahim</h2>
+        </div>
+    );
+};
+
+export default Header;
